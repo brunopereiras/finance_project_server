736d771d78ea594df0939f060703d52e6c8cae46
Bruno Duarte Pereiras Santos <bruno.santos@timeneon.com.br>
2018-02-28 14:16:34 -0300

    Corrigido o layout da aplicação.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'entries#index'
+
   resources :entries
   resources :entry_categories
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
